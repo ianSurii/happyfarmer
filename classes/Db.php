827433fc277c117b1bacf9c9c329fb7fe774ed10
IanSurii <mrkeys254@gmail.com>
2021-03-08 20:56:15 +0300

    heroku

--- classes/Db.php
+++ classes/Db.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 define('servername','us-cdbr-east-03.cleardb.com');
-define('username','heroku_fa93cedb859fa1b');
+define('username','b8c5d1bb453511');
 define('password','483f9647');
 define('dbname','heroku_fa93cedb859fa1b');
 //483f9647@us-cdbr-east-03.cleardb.com heroku_fa93cedb859fa1b
