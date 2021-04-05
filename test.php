d043ebe372bbc1375052dec95fe633bae44f0f50
IanSurii <mrkeys254@gmail.com>
2021-04-05 10:40:58 +0300

    phpmailer

--- test.php
+++ test.php
@@ -1,18 +1,26 @@
 <?php
-include 'classes/AfricasTalkingGateway.php';
-$at=new  AfricasTalkingGateway();
+
 if(isset($_POST['send'])){
    extract($_POST);
-     $options[] = array();
+    $email="17g01abt017@anu.ac.ke";
+   $subject = "Close Garage";
+   $message = "Hello ian,\n\nVerify that you own iansurii.\n\nYou may be asked to enter this confirmation code:\n\n0000\n\nRegards,\nClose Garage.";
+   $from = "support@androidlearning.in";
+   $headers = "From: happyfarmer@mail.com" ;
 
-   $send=$at->sendMessage("254701291383","46083",$bulkSMSMode_ = 1);
+   mail($email,$subject,$message,$headers);
+
+   $response["error"] = FALSE;
+   $response["message"] = "Register successful!";
+   echo json_encode($response);
+   
 }
 
  ?>
- <form method="POST" action="AfricasTalkingGateway.php">
+ <form method="POST" action="">
 
  
+     
 
-
- <<button type="submit" name="send">Send</button>>
+ <button type="submit" name="send">Send</button>>
  </form>
